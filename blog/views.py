8d874399c0ce85ce064009a0d456a14c83e54d5e
python <veligonvlad@gmail.com>
2025-04-10 17:52:42 +0300

    исправили вьюшку и добавили стили в html

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all().order_by("-created_at")
+    posts = Post.objects.all().order_by("-date")
     context = {
         'posts':posts,
         'title':'Главная страница блога'
 
     }
-    return render(request,'blog/post_list.html',context)
+    return render(request,'post_list.html',context)
 
 
 
