4f4b494906f96425b4cc6ade78e123596a13fac7
python <veligonvlad@gmail.com>
2025-04-10 17:45:56 +0300

    сделали кастомную админ панель, создали страницу постов, прописали маршруты, сделали вьюшку

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 from .models import Category, Post
 
 
-admin.site.register(Category)
-admin.site.register(Post)
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title","content","date","category")
+    list_display_links = ("title",)
+    search_fields = ("title",)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_display_links = ("name",)
+    search_fields = ("name",)
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Post,PostAdmin)
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by("-created_at")
+    context = {
+        'posts':posts,
+        'title':'Главная страница блога'
+
+    }
+    return render(request,'blog/post_list.html',context)
+
+
+
+
