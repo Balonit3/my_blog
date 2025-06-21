c65130065619ab64a6c07237d96819e419c8d617
python <veligonvlad@gmail.com>
2025-06-21 12:58:38 +0300

    создали шаблон под авторизацию и регистрацию прописали пути создали функцию во вьюшке для авторизации добавили на главную страницу кнопку вход и регистрацию прописали логику создание и редактирования постов,только авторизованным пользователям

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.all().order_by("-date")
@@ -21,6 +24,8 @@ def post_detail(request,pk):
     }
     return render(request,'post_detail.html',context)
 
+
+@login_required
 def create_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -31,7 +36,7 @@ def create_post(request):
         form = PostForm()
     return render(request,'post_form.html',{'form':form})
 
-
+@login_required
 def edit_post(request,pk):
     post = get_object_or_404(Post,pk=pk)
     if request.method == 'POST':
@@ -48,6 +53,18 @@ def delete_post(request,pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('post_list')
+    else:
+        form = UserCreationForm()
+    return render(request,'registration/signup.html', {'form':form})
 
 
 
